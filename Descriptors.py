f6a0e097d841c2e6dd38f140cfde24524b4a5c37
Hovhannes-Ohanyan <108061081+Hovhannes-Ohanyan@users.noreply.github.com>
2023-07-20 11:41:54 +0400

    implementing descriptor classes in Python to enforce type validation for attributes

---
+++ Descriptors.py
@@ -0,0 +1,97 @@
+
+class IntDescriptor:
+    def __set_name__(self, owner_class, property_name):
+        self.property_name = property_name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.property_name, None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise ValueError(f"{self.property_name} must be integer")
+        instance.__dict__[self.property_name] = value
+
+
+class FloatDescriptor:
+    def __set_name__(self, owner_class, property_name):
+        self.property_name = property_name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.property_name, None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, float):
+            raise ValueError(f"{self.property_name} must be float")
+        instance.__dict__[self.property_name] = value
+
+
+class ListDescriptor:
+    def __set_name__(self, owner_class, property_name):
+        self.property_name = property_name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.property_name, None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, list):
+            raise ValueError(f"{self.property_name} must be list")
+        instance.__dict__.get[self.property_name] = value
+
+
+
+class ValidTypeDescriptor:
+    def __init__(self, expected_type):
+        self.expected_type = expected_type
+
+    def validate_type(self, value):
+        if not isinstance(value, self.expected_type):
+            raise ValueError(f"Expected type  {self.expected_type.__name__}, but got {type(value).__name__}")
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.name, None)
+
+    def __set__(self, instance, value):
+        self.validate_type(value)
+        instance.__dict__[self.name] = value
+
+
+class Person:
+    age = ValidTypeDescriptor(int)
+    height = ValidTypeDescriptor(float)
+    tags = ValidTypeDescriptor(list)
+    favorite_foods = ValidTypeDescriptor(tuple)
+    name = ValidTypeDescriptor(str)
+
+    def __init__(self, age, height, tags, favorite_foods, name):
+        self.age = age
+        self.height = height
+        self.tags = tags
+        self.favorite_foods = favorite_foods
+        self.name = name
+
+
+try:
+    person = Person(age=30, height=175.5, tags=['friendly', 'adventurous'], favorite_foods=('pizza', 'sushi'),
+                    name='John Doe')
+
+
+
+    person.age = "jdn"
+    person.height = '175.5'
+    person.tags = {'friendly', 'adventurous'}
+    person.favorite_foods = 'burger'
+    person.name = 100
+
+except ValueError as e:
+    print(e)
